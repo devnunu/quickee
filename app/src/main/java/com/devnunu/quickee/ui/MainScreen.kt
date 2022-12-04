9efe7cdbd866e2aed9823cda96b5ad37a563b65b
devnunu <aufcl4858@naver.com>
2022-12-04 14:45:57 +0900

    ui: MainScreen preview 추가

--- app/src/main/java/com/devnunu/quickee/ui/MainScreen.kt
+++ app/src/main/java/com/devnunu/quickee/ui/MainScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.devnunu.quickee.theme.QuickeeTheme
 import com.devnunu.quickee.ui.components.QuickeeMainInput
@@ -60,4 +61,10 @@ fun MainScreen(
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
 }
